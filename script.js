90ae5039aea392be72eaf7e49413246e72479545
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-24 15:56:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const value=document.getElementById("count");
+const Increment=document.getElementById("increment");
+Increment.addEventListener('click',(e)=>{
+	let count=parseInt(value.innerText);
+	count=count+1;
+	alert(count);
+	value.innerHTML=count;
+})
